9ff4ab0ca33e64312d184b7aa2771ef9d11bcd5b
masabou4468 <136241246+masabou4468@users.noreply.github.com>
2023-06-13 22:52:22 +0900

    課題　連想配列を作って値とキーを出力しよう

---
+++ kadai_007/associative-array-kadai.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP課題</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $onion_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        print_r($onion_data);
+        ?>
+    </p>
+</body>
+
+</html>
