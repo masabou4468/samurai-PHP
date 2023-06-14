b056e889b637b4139b3a0b419af376628c22eb78
masabou4468 <136241246+masabou4468@users.noreply.github.com>
2023-06-14 11:24:35 +0900

    課題　foreach文を使って連想配列の値とキーを出力しよう

---
+++ kadai_011/foreach-kadai.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP課題</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $onion_data = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
+
+
+        foreach($onion_data as $key => $valuse){
+            echo "{$key}：{$valuse}<br>";
+        }
+        ?>
+    </p>
+</body>
+
+</html>
